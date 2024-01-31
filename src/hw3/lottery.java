63a4dca7c1f525487ce88b224efd3a6f36274812
Emma <down292520@gmail.com>
2024-02-01 01:23:20 +0800

    addhomework

---
+++ src/hw3/lottery.java
@@ -0,0 +1,24 @@
+package hw3;
+
+import java.util.Scanner;
+
+public class lottery {
+	public static void main (String []args) {
+		Scanner sc = new Scanner(System.in);//ctrl+shift+o
+		System.out.println("請輸入1~9討厭的數字");
+		int hatenumber = sc.nextInt();
+		int count =0;
+		
+		for(int number = 1;number<=49;number++) {
+			if(number / 10 == hatenumber ||number % 10 == hatenumber) {
+				continue;
+			}
+			else if(hatenumber!=number) {
+				System.out.println(number);
+				count=count+1;			
+			}				
+		 }System.out.println("總共有"+count+"數字可選");
+		
+	}
+
+}

2369e897639c17d4fe8d7ef68adf2a3ea847c8a1
Emma <down292520@gmail.com>
2024-02-06 19:36:34 +0800

    testRenamed

--- src/hw3/traingleTest.java
+++ src/hw3/traingleTest.java
@@ -2,7 +2,7 @@ package hw3;
 
 import java.util.Scanner;
 
-public class TraingleTest {
+public class traingleTest {
 	public static void main(String []args) {
 		Scanner sc = new Scanner(System.in);
 

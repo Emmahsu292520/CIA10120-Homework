fc684eae96ccf38f64394c577912ea1c3c6e5192
Emma <down292520@gmail.com>
2024-03-01 23:48:40 +0800

    修改功課七內容

--- src/hw7/AnimalRead.java
+++ src/hw7/AnimalRead.java
@@ -14,9 +14,11 @@ public class AnimalRead {
 				while(true) {
 				((Animal)ois.readObject()).speak();
 				}
-				
+				/*當在 ObjectInputStream 中，當呼叫 readObject() 方法讀取序列化檔案時，如果已經到達檔案的結尾，它將拋出一個 EOFException。
+				 因此，我們在 while (true) 迴圈中使用 try-catch 塊來捕捉這個例外。當捕捉到 EOFException 時，表示已經讀取完整個檔案，迴圈結束，程式退出。
+				 */
 			}catch(EOFException a) {
-				System.out.println("資料讀取完畢！");
+				System.out.println("資料讀取完畢！"); 
 			} catch (ClassNotFoundException e) {
 				e.printStackTrace();
 			} catch (IOException e) {
